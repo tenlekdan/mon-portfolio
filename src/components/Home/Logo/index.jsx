e68100f9bce0a5e731ad3d496642335f0d7f2a30
Tenzin M <tenzinm@Tenzins-Air-3.lan>
2024-05-16 10:01:35 -0400

    removed gsap

--- src/components/Home/Logo/index.jsx
+++ src/components/Home/Logo/index.jsx
@@ -1,37 +1,37 @@
 import './index.scss';
 import LogoS from '../../../assets/images/logo-s.png'
 import { useEffect, useRef } from 'react';
-import gsap from 'gsap-trial';
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin';
+// import gsap from 'gsap-trial';
+// import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin';
 export default function Logo() {
     const bgRef = useRef();
     const outlineLogoRef = useRef();
     const solidLogoRef = useRef();
 
     useEffect( ()=> {
-        gsap.registerPlugin(DrawSVGPlugin);
+        // gsap.registerPlugin(DrawSVGPlugin);
 
-        gsap.timeline()
-        .to(bgRef.current, {
-            duration: 1,
-            opacity: 1
-        })
-        .from(outlineLogoRef.current, {
-            drawSVG: 0,
-            duration: 20
-        })
+        // gsap.timeline()
+        // .to(bgRef.current, {
+        //     duration: 1,
+        //     opacity: 1
+        // })
+        // .from(outlineLogoRef.current, {
+        //     drawSVG: 0,
+        //     duration: 20
+        // })
 
-        gsap.fromTo(
-            solidLogoRef.current,
-            {
-                opacity: 0, 
-            },
-            {
-                opacity: 1,
-                delay: 4,
-                duration: 4
-            }
-        )
+        // gsap.fromTo(
+        //     solidLogoRef.current,
+        //     {
+        //         opacity: 0, 
+        //     },
+        //     {
+        //         opacity: 1,
+        //         delay: 4,
+        //         duration: 4
+        //     }
+        // )
     }, [])
     return (
         <div className="logo-container" ref={bgRef}>
